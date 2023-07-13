42e48b0d64cbe9405c84e0825ff5cdb92a2fd508
Marquitoxgo <marcossanchezgo@gmail.com>
2023-07-13 19:28:09 -0300

    front y back

---
+++ js/productos.js
@@ -0,0 +1,49 @@
+
+
+const {createApp} = Vue;
+
+createApp({
+ data(){
+    return{
+        productos: [],
+        url: 'http://127.0.0.1:5000/productos',
+        cargando: true,
+        error: false
+    }
+ },
+
+ methods:{
+    fetchApi(){
+        fetch(this.url)
+        .then(res => res.json())
+        .then(data =>{
+            this.productos = data;
+            this.cargando = false;
+        })
+        .catch(err=>{
+            console.error(err);
+            this.error = true;
+        })
+    },
+
+    eliminar(id){
+        const url = this.url+"/"+id
+        let options = {
+            method: 'DELETE'
+        }
+
+        fetch(url,options)
+        .then(res => res.json())
+        .then(data =>{
+            
+            lacation.reload();
+        })
+        .catch(err => console.error(err))
+    }
+ },
+
+ created(){
+    this.fetchApi(this.url);
+
+ }
+}).mount('#app')
